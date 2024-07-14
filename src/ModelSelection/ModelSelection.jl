1d0a135117cd2349ef772991191ddb57a234c537
ilkerarslan <ilker.arslan@outlook.com>
2024-07-14 15:53:07 +0300

    bug fix

--- src/ModelSelection/ModelSelection.jl
+++ src/ModelSelection/ModelSelection.jl
@@ -2,7 +2,7 @@ module ModelSelection
 
 include("CrossValScore.jl")
 include("LearningCurve.jl")
-include("StratifiedKFold.jl")
+include("StratifiedKFoldModel.jl")
 include("TrainTestSplit.jl")
 
 export cross_val_score, learning_curve, StratifiedKFold, train_test_split
